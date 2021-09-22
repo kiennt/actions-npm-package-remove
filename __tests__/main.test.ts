114cde32428bcec03e416579c4b8a17732b7dd23
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-09-22 09:20:17 +0700

    add test for delete repo from user

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,5 +1,20 @@
+import {deletePackages} from '../src/github'
+
+jest.setTimeout(30000)
+
 describe('test', () => {
   it('should return true', () => {
     expect(1).toEqual(1)
+  })
+
+  it('should delete packages', async () => {
+    await deletePackages({
+      githubToken: '',
+      username: 'hoangviet',
+      repo: 'tiki-miniapp',
+      packages: ['tf-miniapp', 'tf-miniapp-core'],
+      semVerPattern: '1.75.0',
+      dryRun: true
+    })
   })
 })
